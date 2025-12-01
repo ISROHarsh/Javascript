d5513186d09e7af4515be96967b0ee032ee7ec63
Harsh Verma <120256045+ISROHarsh@users.noreply.github.com>
2025-12-01 14:25:31 +0000

    Learnt about Datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict"; //treat all JS code as a new version
+
+// alert(3+3) // we don't use browser, this could be accesed in the inspect page of the browser
+
+console.log(3+3);
+
+console.log("Harsh");
+
+let name = "Harsh"
+let age = 3
+let isloggedin = true
+
+// number => 2 to the power 53 is the range
+// bigint 
+// string => ""
+// boolean => true or false
+// null=> standalone value
+// undefined =>
+// symbol => unique
+
+// object
+
+console.log(typeof undefined);
+console.log(typeof null);
