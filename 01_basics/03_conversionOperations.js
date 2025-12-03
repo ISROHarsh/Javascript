6a3b64640f079a9a7d1007e8d7f40eb333bbdbeb
Harsh Verma <120256045+ISROHarsh@users.noreply.github.com>
2025-12-03 01:05:40 +0000

    Basic Operations

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -1,6 +1,6 @@
-// let score = "Harsh"
+let score = "Harsh"
 // console.log(typeof (score));
-// let  valueInNumber = Number(score)
+let  valueInNumber = Number(score)
 // console.log(typeof (valueInNumber));
 // console.log(valueInNumber);
 
@@ -10,9 +10,7 @@
 // true => 1, false => 0
 
 let isLoggedIn = "Harsh"
-
 let booleanIsLoggedIn =  Boolean(isLoggedIn)
-
 // console.log(booleanIsLoggedIn);
 
 // 1 => true, 0 => false
@@ -21,5 +19,32 @@ let booleanIsLoggedIn =  Boolean(isLoggedIn)
 
 let someNumber = 33
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber)
+
+// Operations
+
+let value = 3
+let negValue = -(value)
+// console.log(negValue);
+
+let str1 = "Harsh"
+let str2 = "Hello "
+
+let str3 = str2 + str1
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log(1 + 2 + "2");
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
