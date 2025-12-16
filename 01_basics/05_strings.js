d8826d52d5f8e887a41bb8381d9feb43bd611a4f
Harsh Verma <120256045+ISROHarsh@users.noreply.github.com>
2025-12-16 05:53:21 +0000

    Learnt strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,24 @@
+const name = "Harsh"
+const repoCount = 3
+// console.log(`My name is ${name} & My repo count is ${repoCount}`);
+
+const gameName = new String('ISRO-Harsh')
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+const newString = gameName
+// console.log(newString.substring(0,4))
+
+const anotherString = gameName.slice(-8,7)
+// console.log(anotherString);
+
+const newStringOne =  "  Harsh  "
+// console.log(newStringOne);
+// console.log(newStringOne.trim());
+
+const url = "https://ISROHarsh.com/harsh%20verma"
+// console.log(url.replace('%20','-'));
+
+// console.log(url.includes('harsh'));
+console.log(gameName.split('-'));
