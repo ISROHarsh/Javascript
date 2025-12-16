b3326c567ef23a2d04132dc73c993b77e08066e5
Harsh Verma <120256045+ISROHarsh@users.noreply.github.com>
2025-12-16 06:53:45 +0000

    Date and Time

---
+++ 01_basics/07_datesinJS.js
@@ -0,0 +1,17 @@
+// Dates
+
+let myDate = new Date()
+// console.log(myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toJSON());
+
+let newCreatedDate = new Date(2026, 0, 1)
+// console.log(newCreatedDate.toDateString());
+
+let myTimeStamp = Date.now()
+// console.log(myTimeStamp);
+// console.log(newCreatedDate.getTime());
+// console.log(Math.floor(Date.now()/1000));
