81f1e95ab4c6aec44fb81ccc912a8cfe29f817e3
Harsh Verma <120256045+ISROHarsh@users.noreply.github.com>
2025-12-23 05:50:56 +0000

    learnt switch-case

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,29 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = 3
+
+switch (month) {
+    case 1:
+        console.log("Month is Jan")
+        break;
+    case 2:
+        console.log("Month is Feb")
+        break;
+    case 3:
+        console.log("Month is Mar")
+        break;
+    case 4:
+        console.log("Month is Apr")
+        break;
+
+    default:
+        console.log("Default got matched");
+        break;
+}
