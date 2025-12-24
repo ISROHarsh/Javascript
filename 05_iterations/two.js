abe91559c01b8e77113b70eed9aa63fb86badd1a
Harsh Verma <120256045+ISROHarsh@users.noreply.github.com>
2025-12-24 00:48:49 +0000

    While and do While Loops

---
+++ 05_iterations/two.js
@@ -0,0 +1,21 @@
+// While
+
+let index = 0
+// while (index <= 10) {
+//     // console.log(`value of index is ${index}`);
+//     index += 2
+// }
+
+myArray = ['flash', 'superman', 'batrman']
+while (index < myArray.length) {
+    // console.log(`Value of index is ${myArray[index]}`);
+    index ++
+}
+
+
+let score  = 11
+
+do {
+    console.log(`score is ${score}`);
+    score ++
+} while (score <= 10);
