feaaff2f4055f1effad3bc3a1304590b024db63a
ISROHarsh <gamoaster@gmail.com>
2025-12-25 13:32:48 +0530

    Learnt for of loop

---
+++ 05_iterations/three.js
@@ -0,0 +1,39 @@
+// for of
+
+// ["","",""]
+// [{},{},{}]
+
+// const arr = [1, 2, 3, 4, 5]
+
+// for (const num of arr) {
+//     console.log(num);
+// }
+
+// const greetings = "Hello World"
+// for (const greet of greetings) {
+//     console.log(`each char is ${greet}`); }
+
+
+// Maps - objects with key and value player but remain in order and no duplicate values are here only unique ones
+
+// const map = new Map()
+// map.set('IN','India')
+// map.set('USA','United States of America')
+// map.set('FR','France')
+
+// console.log(map);
+
+// for (const [key,values] of map) {
+//     console.log(key," - ",values);  
+// }
+
+
+const myObject = {
+    game : 'nfs',
+    game2 : 'spiderman'
+} 
+
+// for (const [key,value] of myObject) {
+//     console.log(key,'-',value);
+    
+// }
